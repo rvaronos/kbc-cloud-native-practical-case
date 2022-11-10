e9a7661d8dee41227bc0896c7f47ffcb3a52a6dd
Rafail Varonos <rafail.varonos@kbc.be>
2022-11-10 10:51:30 +0100

    create test files

---
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/api/cocktail/CocktailControllerTest.java
@@ -0,0 +1,10 @@
+package com.ezgroceries.shoppinglist.api.cocktail;
+
+import org.junit.jupiter.api.Test;
+
+public class CocktailControllerTest {
+    @Test
+    void testCocktails() {
+
+    }
+}
---
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/api/shoppinglist/ShoppingListControllerTest.java
@@ -0,0 +1,20 @@
+package com.ezgroceries.shoppinglist.api.shoppinglist;
+
+import org.junit.jupiter.api.Test;
+
+public class ShoppingListControllerTest {
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testGet() {
+
+    }
+
+    @Test
+    void testGetAll() {
+
+    }
+}
