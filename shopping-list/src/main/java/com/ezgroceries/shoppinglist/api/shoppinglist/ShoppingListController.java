efdfcba86e665f88926cfe31ab2f961168b16085
Rafail Varonos <rafail.varonos@kbc.be>
2022-11-10 10:40:00 +0100

    get shopping list

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/api/shoppinglist/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/api/shoppinglist/ShoppingListController.java
@@ -3,11 +3,11 @@ package com.ezgroceries.shoppinglist.api.shoppinglist;
 import java.net.URI;
 import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 public class ShoppingListController {
 
 	@PostMapping(value = "/shopping-lists")
-	@ResponseStatus(code = HttpStatus.CREATED)
 	public ResponseEntity<Void> create(@RequestBody ShoppingListCreateRequestBody body) {
 		ShoppingList createdShoppingList = new ShoppingList();
 		createdShoppingList.setShoppingListId(UUID.randomUUID());
@@ -27,6 +26,23 @@ public class ShoppingListController {
 				.buildAndExpand(createdShoppingList.getShoppingListId()).toUri();
 
 		return ResponseEntity.created(location).build();
+
+	}
+
+	@GetMapping(value = "/shopping-lists/{shoppingListId}")
+	public ShoppingList get(@PathVariable UUID shoppingListId) {
+		ShoppingList fetchedShoppingList = new ShoppingList();
+		fetchedShoppingList.setShoppingListId(UUID.randomUUID());
+		fetchedShoppingList.setName("Stephanie's birthday");
+		fetchedShoppingList.setIngredients(new String[] {
+				"Tequila",
+				"Triple sec",
+				"Lime juice",
+				"Salt",
+				"Blue Curacao"
+		});
+
+		return fetchedShoppingList;
 
 	}
 
